2e3923388a2d2941c9497e0b09dca99a90ecada6
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-01-29 11:28:51 +0100

    Finished index.js up to #7

--- js/index.js
+++ js/index.js
@@ -1,1 +1,65 @@
-// Your code goes here
+// 1. On mouseover, have `.container .intro img` scale to 1.1 and return to normal scale on mouseout.
+const funBus = document.querySelector('.home .intro img')
+funBus.addEventListener('mouseover', function (){
+    funBus.style.transform = "scale(1.1)";
+})
+funBus.addEventListener('mouseout', function (){
+    funBus.style.transform = "scale(1)";
+})
+
+// 2. Whenever ANY key is pressed, turn the body's background into a gradient
+const body = document.querySelector('body')
+body.addEventListener('keydown', function () {
+    body.style.backgroundImage = "linear-gradient(#bdc3c7,#2c3e50)"
+})
+body.addEventListener('keyup', function () {
+    body.style.backgroundImage = "none"
+})
+
+// 3. When the wheel is used, turn all text to red.
+const introP = document.querySelector('.intro p')
+introP.addEventListener('wheel', function (e) {
+    introP.style.color = "red";
+    e.stopPropagation();
+})
+
+// 4. Drag the funBus img and append it to one of the two .content-section divs.
+const containers = document.getElementsByClassName('content-section')
+
+for (const container of containers) {
+    container.addEventListener("dragover", dragover)
+    container.addEventListener("dragenter", dragenter)
+    container.addEventListener("drop", drop)
+}
+
+function dragover (e) {e.preventDefault()}
+function dragenter (e) {e.preventDefault()}
+function drop () {this.append(funBus);}
+
+// 5. Console.log("OMG THE IMAGE ACTUALLY LOADED! YOU'RE A WIZARD, 'Arry") when the img loads.
+funBus.addEventListener('load', (() => {
+    console.log("OMG, THE IMAGE ACTUALLY LOADED! You're a wizard 'Arry!")
+}))
+
+// 6. When focusing on one of the destination divs, add a 1px solid red border
+const destinations = document.querySelector('.myDestination');
+destinations.addEventListener('focus', function () {
+    destinations.textContent= "This is still a work in progress. Thanks for your patience!";
+})
+
+// 7. when the window is resized, change the color of the 'Fun Bus' header
+const header = document.querySelector('header .nav-container h1');
+window.addEventListener('resize', (() => {
+    console.log("It's working!")
+    header.style.color = "green"
+}))
+
+// 8. When scrolling, don't display any content on the body
+
+// 9.
+
+// 10.
+
+
+
+
