4752fbc0c4729edc35ad6e671db8f3ee675dfa1a
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-02-12 15:36:20 +0100

    Added functionality to sort blocks back on top

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,15 +1,13 @@
 const blockholder = document.querySelector('.blocks');
 const blocks = document.querySelectorAll('.block');
 
+// Add event listeners for click and hold to blocks.
 blocks.forEach((block) => {
     block.addEventListener('click', ((e) => {
         backToTop(e);
     }))
 })
-
+// When a block is clicked, it is prepended to blockholder while the other elements get push down
 const backToTop = (e) => {
     blockholder.prepend(e.target);
 }
-
-// Add event listeners for click to blocks.
-// When a block is clicked, it ought to be taken from the blocks NodeList and then prepended to it.
